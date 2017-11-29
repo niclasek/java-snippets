4d7a180e0fe69287a5b70358c31346cb093b9cbc
niclas <niclas.l.ek@gmail.com>
2017-11-29 13:15:02 +0100

    Added some more logging options

--- logging/Main.java
+++ logging/Main.java
@@ -14,5 +14,18 @@ public class Main {
 		logger.log(Level.WARNING, "A warning log message");
 		logger.log(Level.INFO, "An info log message");
 		logger.log(Level.FINE, "A fine log message");
+		
+		//A more convenient way of logging
+		logger.warning("Another way to log a warning message");
+		logger.info("An info log message");
+		logger.fine("A fine log message");
+		
+		//Precise logging method
+		//It doesn't rely on the call stack for the class and method names
+		logger.logp(Level.SEVERE, "logging.Main", "main", "A precise log message");
+		
+		//Parameterized message methods
+		logger.log(Level.INFO, "{0} is my favorite color", "green");
+		logger.log(Level.INFO, "{0} is {1} days from {2}", new Object[] {"wed", 2, "Fri"});
 	}
 }
